4aa712b0013cb97217e041dd5a723ecf6be7d7bd
tsukurukids <tsukurukids4f@gmail.com>
2026-04-25 09:48:40 +0900

    a

---
+++ sw.js
@@ -0,0 +1,31 @@
+// サービスワーカー：オフラインでも動くようにファイルを保存するロボットのようなものだよ！
+
+const CACHE_NAME = 'odai-gacha-v1';
+const urlsToCache = [
+  'index.html',
+  'style.css',
+  'script.js',
+  'manifest.json'
+];
+
+// インストールされたときに、ファイルをキャッシュ（保存）するよ
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('ファイルを覚えたよ！');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// インターネットからファイルを持ってくる代わりに、保存したファイルを使うよ
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 保存されていたらそれを使うし、なければインターネットに取りに行くよ
+        return response || fetch(event.request);
+      })
+  );
+});
